bf1d884ae4982e6cad6828e35215c9cbc39e9ba1
Maya Flynn <flynnmay@grinnell.edu>
2024-04-01 19:46:39 -0500

    Added Tests

--- src/SortTester.java
+++ src/SortTester.java
@@ -45,4 +45,54 @@ public class SortTester {
     assertArrayEquals(original, expected);
   } // orderedStringTest
 
+  @Test
+  public void arbitrarilyOrderedStringTest() {
+    String[] original = { "bravo", "delta", "foxtrot", "charlie", "alpha" };
+    String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void sortWithDuplicateValuesStringTest() {
+    String[] original = { "charlie", "foxtrot", "delta", "bravo", "foxtrot" };
+    String[] expected = { "bravo", "charlie", "delta", "foxtrot", "foxtrot" };
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void orderedIntegerTest() {
+    Integer[] original = {0, 1, 2, 3, 4, 5, 6};
+    Integer[] expected = {0, 1, 2, 3, 4, 5, 6};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void reverseOrderedIntegerTest() {
+    Integer[] original = {6, 5, 4, 3, 2, 1, 0};
+    Integer[] expected = {0, 1, 2, 3, 4, 5, 6};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void arbitrarilyOrderedIntegerTest() {
+    Integer[] original = {4, 6, 0, 5, 1, 2, 3};
+    Integer[] expected = {0, 1, 2, 3, 4, 5, 6};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+  @Test
+  public void sortWithDuplicateValuesIntegerTest() {
+    Integer[] original = {5, 1, 6, 1, 4, 5, 1};
+    Integer[] expected = {1, 1, 1, 4, 5, 5, 6};
+    sorter.sort(original, (x, y) -> x.compareTo(y));
+    assertArrayEquals(original, expected);
+  } // orderedStringTest
+
+
+
 } // class SortTester
